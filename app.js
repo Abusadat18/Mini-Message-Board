aa7273fe95773dc94476034884de3608c5dd29d3
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-08-12 07:31:43 +0530

    Add build script

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/",indexRouter)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at PORT ${process.env.PORT}`);
+    
+})
